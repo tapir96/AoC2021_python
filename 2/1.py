6115270b55d8304c289e5abcb316afb6b21facdc
dangzome <daniel.t.zhang@ntnu.no>
2021-12-06 17:39:43 +0100

    q2p1d

---
+++ 2/1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+d = np.loadtxt('input', dtype='str')
+dic = {'forward': 0, 'up': 0, 'down': 0}
+for i in d:
+    dic[i[0]] += int(i[1])
+
+print(dic['forward'], dic['down'], dic['up'], dic['forward']*(dic['down']-dic['up']))
+
+
