ec8757332ce6e8f1272cd2aef2d707a81dd84855
dangzome <daniel.t.zhang@ntnu.no>
2021-12-06 18:33:55 +0100

    clean

--- 3/1.py
+++ 3/1.py
@@ -4,10 +4,6 @@ d = np.loadtxt('input0', dtype='str')
 l0, l1, k = len(d[0]), len(d), ''.join(d)
 o = ['1' if k[i::l0].count('1') > l1/2 else '0' for i in range(l0)]
 
-print(''.join(o))
-print(''.join(['0' if i == '1' else '1' for i in o]))
-print(''.join([str(int(i)^1) for i in o]))
-print(int(''.join(['0' if i == '1' else '1' for i in o]),2)*int(''.join(o),2))
 print(int(''.join([str(int(i)^1) for i in o]),2)*int(''.join(o),2))
 
 
