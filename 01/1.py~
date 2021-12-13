692752f125d88b855db1a06396e64250ad4ed8ad
dangzome <daniel.t.zhang@ntnu.no>
2021-12-13 17:39:46 +0100

    uh

--- 01/1.py~
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-d1 = np.loadtxt('input')
-print(np.sum(np.diff(d1)>0))
-
-
--- 03/1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-d = np.loadtxt('input', dtype='str')
-l0, l1, k = len(d[0]), len(d), ''.join(d)
-o = ['1' if k[i::l0].count('1') > l1/2 else '0' for i in range(l0)]
-
-print(int(''.join([str(int(i)^1) for i in o]),2)*int(''.join(o),2))
-
-
