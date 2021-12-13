0391a7172174b6b99e49c3727f26d4dd8933b531
dangzome <daniel.t.zhang@ntnu.no>
2021-12-13 17:44:05 +0100

    clean

--- 06/1.py
+++ 06/1.py
@@ -2,13 +2,11 @@ import numpy as np
 
 # to get np.array of int
 d = np.array([int(i) for i in str(np.loadtxt('input', dtype='str')).split(',')])
-days = 256
+days, l = 256, [sum(d==i) for i in range(9)]
 
-l = [sum(d==i) for i in range(9)]
 for i in range(days):
-    temp = l.pop(0)
-    l.append(temp)
-    l[6] += temp
+    l.append(l.pop(0))
+    l[6] += l[-1]
 
 print(sum(l))
 
