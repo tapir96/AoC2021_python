b07a35bae9ccc16e8fde03caa7500b75a02ecaef
dangzome <daniel.t.zhang@ntnu.no>
2021-12-14 10:12:17 +0100

    simplify

--- 07/2.py
+++ 07/2.py
@@ -2,10 +2,9 @@ import numpy as np
 
 # to get np.array of int
 d = np.array([int(i) for i in str(np.loadtxt('input', dtype='str')).split(',')])
-l, h, f0 = min(d), max(d), []
-arr = np.cumsum(range(h-l+1))
+f0, arr = [], np.cumsum(range(max(d)-min(d)+1))
 
-for i in range(h-l):
+for i in range(len(arr)):
     f1 = [arr[abs(j-i)] for j in d]
     f0.append(sum(f1))
 
