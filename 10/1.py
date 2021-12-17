8bfd23a57dba3cc82133f104fe5b39f7087071f2
dangzome <daniel.t.zhang@ntnu.no>
2021-12-17 21:12:19 +0100

    cleaned

--- 10/1.py
+++ 10/1.py
@@ -1,63 +1,21 @@
 import numpy as np
 
-d = np.loadtxt('input', dtype='str')
-brac = '()<>[]{}'
-l_bra = {brac[i] : brac[i+1] for i in range(len(brac))[::2]}
-r_bra = {brac[i] : brac[i-1] for i in range(len(brac))[1::2]}
-bra_d = {')': 3, ']': 57, '>': 1197, '}': 25137} 
+d, bracs = np.loadtxt('input', dtype='str'), '()[]{}<>'
+bra_d = {i[0]: i[1] for i in zip(bracs[1::2], [3, 57, 1197, 25137])}
+l_bra = {bracs[i]: bracs[i+1] for i in range(len(bracs))[::2]}
+r_bra = {v: k for k, v in l_bra.items()}
 
-zopa = []
+brac = []
 for i in d:
     i_mod = list(i)
-    print(i)
-    for j in range(int(len(i)/2)):
+    for _ in range(len(i)):
         l_rm = []
         for x, y in enumerate(i_mod[:-1]):
             if l_bra.get(y) == i_mod[x+1]:
                 l_rm += [x, x+1]
         i_mod = [i_mod[k] for k in range(len(i_mod)) if k not in l_rm]
-#                i_mod[x] = '*'
-#                i_mod[x] = '*'
-        print(''.join(i_mod))
-#        for x, y in enumerate(i_mod[:-1-2*j]):
-#            if l_bra.get(y) == i_mod[x+1+2*j]:
-#                if i_mod[1+x:x+1+2*j].count('*') == len(i_mod[1+x:x+1+2*j]):
-#                    i_mod[x] = '*'
-#                    i_mod[x+1+2*j] = '*'
-    for j in i_mod:
-        if r_bra.get(j):
-            zopa.append(j)
-            print(j, 'kakakakkakakakaa')
+        if not l_rm:
+            brac += [[j for j in i_mod if j not in bracs[::2]]]
             break
-    print(' ')
 
-
-#    print(i)
-#    print(''.join(i_mod), zopa[-1], len(zopa))
-#    print(' ')
-#    exit('a')
-#        print(i)
-#        print(i_mod)
-#        print(''.join(i_mod))
-#        print(int(len(i)/2))
-#        if k == 11:
-#            exit('')
-#    print(''.join(i_mod))
-#    print(r_bra)
-#    exit('-')
-
-#    i_mod = ['*' if l_bra.get(i[x]) == i[x+1] else y for x, y in enumerate(i[:-1])]
-#    i_mod += [i[-1]]
-#    for j in reversed(range(len(i_mod[:-1]))):
-#        if i_mod[j] == '*':
-#            i_mod[j+1] = '*'
-
-print(zopa)
-points = 0
-for i in zopa:
-    points += bra_d[i]
-print(points)
-#    print(i)
-#    print(''.join(i_mod))
-#    print(' ')
-#    exit('a')
+print(sum([bra_d.get(i[0], 0) for i in brac if i]))
