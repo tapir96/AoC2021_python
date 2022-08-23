3127115debc6fad9bf3950a8e3f343e9d38ef2e9
dz24 <danielzh@stud.ntnu.no>
2022-08-23 17:27:30 +0200

    a mess

--- 12/2.py
+++ 12/2.py
@@ -29,40 +29,14 @@ def generate_paths(path_dic, path):
             paths.append(path + [route])
             continue
 
-        if len(path) == 6 and path[2] == 'b' and 'c' in path and 'A' in path:
-            print('--- bogi 1 ---')
-            print(path, route, routes)
-            print(small)
-            print(route.islower(), 2 in small.values(), small[route] in (1, 2))
-            print(route.islower() and 2 in small.values() and small[route] in (1, 2))
-            print('--- bogi 2 ---')
 
         if route.islower() and 2 in small.values() and small[route] in (1, 2):
             continue
         
         elif route.islower() and route not in ('start', 'end'):
-            if small[route] == 2:
-                print(' ---- ')
-                print(path, route)
-                print('small', small)
-                print(route.islower(), 2 in small.values(), route in small.keys(), small[route] != 2)
-                print(route in small.keys(), path, list(small.keys()))
-                print(' ---- ')
-                exit('z')
             small[route] += 1
 
-        if sum(['b'== kk for kk in path]) == 2 and route == 'b':
-            print(' ---- ')
-            print(path, route)
-            print('small', small)
-            print(route.islower(), 2 in small.values(), route in small.keys(), small[route] != 2)
-            print(route in small.keys(), path, list(small.keys()))
-            print(' ---- ')
-            exit('hike')
         paths.append(path + [route])
-        if len(path) == 6 and path[2] == 'c' and 'b' in path and 'A' in path:
-            print(path + [route])
-            print('--- bogi 3 ---')
 
     return paths
 
@@ -77,8 +51,6 @@ while paths0:
         paths1 = generate_paths(path_dic, path)
         paths2 += paths1
     print('bang')
-    for zzz in paths2:
-        print(','.join(zzz))
     for idx, path in enumerate(paths2):
         if 'end' in path:
             paths.append(path)
@@ -86,7 +58,7 @@ while paths0:
     paths2 = []
 
     counter += 1
-    if counter == 14:
+    if counter == 18:
         print('               ')
         for path in paths:
             print(path)
